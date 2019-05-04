ecf91e7a967ec09fb1af365020f8c767a667842e
Nic Schellenbaum <nic.schellenbaum@gmail.com>
2019-05-04 23:18:10 +0200

    Design start

--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,11 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_3_clicked();
+
+    void on_quitButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
